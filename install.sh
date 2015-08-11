25b7520f3b6ea37e032c5fd0ca21cb06d5eaaa6a
Remigijus <achmudass@gmail.com>
2015-08-11 22:16:11 +0300

    install.sh beta implementation

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+PYTHON_PATH=$(whereis python)
 
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-(crontab -l ; echo "0 0,4,8,12,16,20 * * * python $DIR/Downloader.py") | crontab
+if [[ $PYTHON_PATH ]]; then
+	DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+	(crontab -l ; echo "0 0,4,8,12,16,20 * * * python $DIR/Downloader.py") | crontab
+else
+	echo 'PLEASE INSTALL PYTHON'
+fi
